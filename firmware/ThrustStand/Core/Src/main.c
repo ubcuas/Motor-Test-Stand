84f7c4f56de7ce25f0f2ce8e071c2618aa1bd830
CharlesSurianto <charlessurianto@gmail.com>
2023-11-12 20:59:38 -0800

    comments

--- firmware/ThrustStand/Core/Src/main.c
+++ firmware/ThrustStand/Core/Src/main.c
@@ -610,11 +610,11 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void HX717_Init(void)
 {
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 10);
-    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
-    HAL_Delay(1);
-    HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 5);
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 10); // 100% duty cycle
+    HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);         // start pwm
+    HAL_Delay(1);                                     // wait
+    HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);          // stop pwm
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 5);  // 50% duty cycle
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
