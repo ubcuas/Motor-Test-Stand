ccfcaafc637716662efadb8223655f65bea54ab1
CharlesSurianto <charlessurianto@gmail.com>
2023-11-25 21:01:30 -0800

    remove commented

--- firmware/v1/Core/Src/main.c
+++ firmware/v1/Core/Src/main.c
@@ -626,7 +626,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
         if (HAL_GPIO_ReadPin(GPIOB, CELL1_Pin | CELL2_Pin | CELL3_Pin | CELL4_Pin | CELL5_Pin | CELL6_Pin) == 0 && PulseCounter == 0)
         {
             // read channel A with 128 gain (HX717 datasheet)
-            // HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t *)ClockPulses, 25);
             HAL_TIM_PWM_Start_IT(&htim3, TIM_CHANNEL_1);
         }
 
